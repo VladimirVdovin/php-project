07d47e78a1e895c6f827e7a0933172c08167ff96
Vladimir Vdovin <drumtamtamorchestra@gmail.com>
2023-10-03 23:17:47 +0200

    First commit

---
+++ code.php
@@ -0,0 +1,178 @@
+<?php
+	echo '<h3>Переменные</h3>';
+	$num = 123;
+	echo $num . '<br>';
+    
+    $a = 10;
+    echo $a . '<br>';
+    $a = 20;
+    echo $a . '<br>';
+
+    echo '<h3>Математические операции</h3>';
+    $a = 1 + 2 + 3;
+    echo $a  . '<br>';
+
+    $a = 10;
+    $b = 2;
+    echo $a + $b . '<br>';
+    echo $a - $b . '<br>';
+    echo $a * $b . '<br>';
+    echo $a / $b . '<br>';
+
+    $c = 10;
+    $d = 5;
+    $res = $c + $d;
+    echo $res . '<br>';
+
+    $a = 1;
+    $b = 2;
+    $c = 3;
+    echo $a + $b + $c . '<br>';
+    
+    $a = 10;
+    $b = 5;
+    $c = $a - $b;
+    $d = 7;
+    $res = $c + $d;
+    echo $res . '<br>'; 
+
+    echo '<h3>Дроби и отрицательные числа</h3>';
+
+    $a = 1.5;
+    $b = 0.75;
+    echo $a + $b . '<br>';
+
+    $a = -100;
+    echo $a . '<br>';
+
+    $a = 200;
+    echo -$a . '<br>';
+
+    echo '<h3>Остаток от деления и возведение в степень</h3>';
+
+    $a = 10;
+    $b = 3;
+    echo $a % $b . '<br>';
+
+    echo 2 ** 10 . '<br>';
+
+    echo '<h3>Строки, сложение строк, strlen, mb_strlen</h3>';
+    $str = 'hello';
+    echo $str . '<br>';
+
+    $str1 = 'abc';
+	$str2 = 'def';
+	echo $str1 . $str2 . '<br>';
+
+    $str1 = 'hello';
+	$str2 = 'world';
+	echo $str1 . ' ' . $str2 . '<br>';
+
+    $str = 'hello';
+    echo strlen($str)  . '<br>';
+
+    $str = 'Привет мир';
+    echo mb_strlen($str)  . '<br>';
+
+    echo '<h3>HTML теги</h3>';
+    echo '<i>Курсив</i>' . '<br>';
+    echo 1 . '<br>';
+    echo 2 . '<br>';
+    echo 3 . '<br>';
+    echo 4 . '<br>';
+    echo 5 . '<br>';
+    echo 6 . '<br>';
+    echo 7 . '<br>';
+    echo 8 . '<br>';
+    echo 9 . '<br>';
+
+    echo "<img src='images.png'>"  . '<br>';
+    echo '<br>';
+    echo "<input value='Какой-нибудь текст'>" . '<br>';
+    echo '<br>';
+    echo '<textarea>' . 'Какой-нибудь текст' . '</textarea>' . '<br>';
+
+    echo '<h3>Логические значения</h3>';
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Моя первая программа</title>
+	</head>
+	<body>
+		<?php
+			error_reporting(E_ALL);
+			ini_set('display_errors', 'on');
+			mb_internal_encoding('UTF-8');
+
+			include 'code.php';
+
+		?>
+	</body>
+</html>
